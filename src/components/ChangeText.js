184c4d4ddfb9da50e34d4e33a00be321e1cf0cd6
ksidedev <snoobeck75@gmail.com>
2018-03-02 18:04:02 +1300

    feat(build): Updated folder structure

--- src/components/ChangeText.js
+++ src/components/ChangeText.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { ChangeTextRedux } from './../actions'
+import { ChangeTextRedux } from './../shared/actions'
 
 class Three extends Component {
 
