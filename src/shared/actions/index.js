13c4179ba671560175ce5fe2f15270a876fb8e23
ksidedev <snoobeck75@gmail.com>
2018-03-05 11:49:23 +1300

    feat(build): Refactoring

--- src/shared/actions/index.js
+++ src/shared/actions/index.js
@@ -1,18 +1,3 @@
-export const HELLO_WORLD = 'HELLO_WORLD'
-export const RESET = 'RESET'
-
-// export const helloWorld = () => {
-//   return {
-//     type: HELLO_WORLD
-//   }
-// }
-
-export const reset = () => {
-  return {
-    type: RESET
-  }
-}
-
 export const ChangeTextRedux = hello => ({
   type: 'COMPONENT_TWO_REDUX',
   hello
